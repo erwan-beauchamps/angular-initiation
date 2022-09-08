c2c49acf92d6e0a5c1fdc0266679fa7674b26d92
erwan-beauchamps <49027437+erwan-beauchamps@users.noreply.github.com>
2022-09-08 14:51:08 +0300

    clean
    clean

--- src/app/first-component/first-component.module.ts
+++ src/app/first-component/first-component.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FirstComponentComponent } from './first-component.component';
 import { MatCommonModule } from '../mat-common.module';
-import { FirstPipePipe } from '../first-pipe.pipe';
 import { SharedPipeModule } from '../shared-pipe.module';
 
 @NgModule({
