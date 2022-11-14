6349f32742c1c3604ac9dfa817a52bfc530c1d26
erwan-beauchamps <49027437+erwan-beauchamps@users.noreply.github.com>
2022-11-14 15:54:19 +0100

    first tests

--- src/app/first-component/first-component.component.spec.ts
+++ src/app/first-component/first-component.component.spec.ts
@@ -19,4 +19,22 @@ describe('FirstComponentComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('isShowed should be false', () => {
+    component.isShowed = true;
+    component.display();
+    expect(component.isShowed).toBeFalse();
+  });
+
+  it('switchValue should be testValue', () => {
+    let testValue = "testValue";
+    component.changeSwitchValue(testValue);
+    expect(component.switchValue).toEqual(testValue);
+  });
+
+  it('isShowed should be true', () => {
+    component.isShowed = false;
+    component.display();
+    expect(component.isShowed).toBeTrue();
+  });
 });
