7702da2bf9c7c372188d2bc79d4281647a3c16e9
erwan-beauchamps <49027437+erwan-beauchamps@users.noreply.github.com>
2023-04-06 23:47:22 +0200

    solution

--- src/app/first-component/first-component.component.ts
+++ src/app/first-component/first-component.component.ts
@@ -2,11 +2,6 @@ import { MessagesService } from './../services/messages.service';
 import { Component, OnInit } from '@angular/core';
 import { SimpleServiceService } from '../services/simple-service.service';
 
-export enum SwitchList {
-  FIRST = "first",
-  SECOND = "second"
-}
-
 @Component({
   selector: 'app-first-component',
   templateUrl: './first-component.component.html',
@@ -14,39 +9,9 @@ export enum SwitchList {
 })
 export class FirstComponentComponent implements OnInit {
 
-  public isShowed: boolean = false;
-  public switchList = SwitchList;
-  public list: string[] = [];
-  public messageList: string[] = [];
-  public buttonList: string[] = ["Exemple de bouton possible", "On peut aussi ecrire ça", "Bonjour à tous",
-    "Avec plaisir", "Merci beaucoup"];
-  public switchValue: string = SwitchList.FIRST;
-
-  constructor(private simpleService: SimpleServiceService, private messageService: MessagesService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.list = this.simpleService.getList();
-    this.messageList = this.messageService.getList();
-  }
-
-  display(): void {
-    this.isShowed = !this.isShowed;
-  }
-
-  changeSwitchValue(newValue: string): void {
-    this.switchValue = newValue;
-  }
-
-  add(): void {
-    this.simpleService.addElement("New element");
-  }
-
-  delete(): void {
-    this.simpleService.deleteLastElement();
-  }
-
-  deleteMessage(message: string): void {
-    this.messageService.deleteMessage(message);
   }
 
 }
--- src/app/second-page/second-page.component.ts
+++ src/app/second-page/second-page.component.ts
@@ -2,8 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { MessagesServerService } from '../services/messages-server.service';
 
-export function firstValidator(input: FormControl) {
-  return (input.value.length > 5) ? null : {firstValidator: true};
+export interface BoxMessage {
+  author: string;
+  message: string;
+}
+
+export enum BoxMessageError {
+  NOT_FOUND = 'not_found',
+  ERROR_404 = 'error_404',
+  WRONG_PWD = 'wrong_pwd',
+  SAME_AUTHOR = 'Auteur et message identique'
 }
 
 @Component({
@@ -15,21 +23,51 @@ export class SecondPageComponent implements OnInit {
 
   messageForm: FormGroup;
   private url: string = 'https://angular-initiation-default-rtdb.firebaseio.com/messages.json';
+  authorError = '';
+  localBoxMessageError = BoxMessageError;
 
   constructor(private _formBuilder: FormBuilder, private _messagesServerService: MessagesServerService) {
     this.messageForm = this._formBuilder.group({
-      author: new FormControl('', [Validators.required, firstValidator]),
+      author: new FormControl('', [Validators.required]),
       message: new FormControl('', [Validators.required]),
     });
   }
 
   ngOnInit(): void {
+    this.initValueChange();
   }
 
   submitForm(): void {
     if (this.messageForm.valid) {
-      this._messagesServerService.postMessages(this.messageForm.value);
+      console.log('value => ', this.messageForm.value);
     }
+  }
+
+  initValueChange() {
+    this.messageForm.valueChanges.subscribe((values: BoxMessage) => {
+      console.log('valueChanges =>', values);
+      let isInvalid = true;
+      if (values.author === BoxMessageError.NOT_FOUND) {
+        this.authorError = BoxMessageError.NOT_FOUND;
+      } else if (values.author === BoxMessageError.ERROR_404) {
+        this.authorError = BoxMessageError.ERROR_404;
+      } else if (values.author === BoxMessageError.WRONG_PWD) {
+        this.authorError = BoxMessageError.WRONG_PWD;
+      } else if (values.author === values.message) {
+        this.authorError = BoxMessageError.SAME_AUTHOR;
+      } else {
+        this.authorError = '';
+        isInvalid = false;
+      }
+      if (this.authorError) {
+        const errors = this.messageForm.controls['author'].errors || {};
+        this.messageForm.controls['author'].setErrors({
+          'authorError': null,
+          ...errors
+        })
+      }
+      console.log('this.errorForm =>', this.authorError);
+    });
   }
 
 }
